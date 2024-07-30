35e5973d262d8d32c1f9626e748db4bec419330c
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-07-30 15:01:48 +0900

    개발환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Timer from "./Timer";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Timer />
+      </header>
+    </div>
+  );
+};
+
+export default App;
