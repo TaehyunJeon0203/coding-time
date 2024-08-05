e439eb9b24e01dc03164296ae04f2e17c25ef2bb
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-08-05 17:39:59 +0900

    Feat: Vscode 활성화 시 작동하는 타이머 추가(프론트 개발 미완료)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,55 @@
 import { app, BrowserWindow } from "electron";
+import { exec } from "child_process";
+import fs from "fs";
 import * as path from "path";
 
 let mainWindow: BrowserWindow | null;
+let intervalId: NodeJS.Timeout;
+const TIMER_FILE_PATH = path.join(app.getPath("userData"), "timer.json");
+
+interface TimerState {
+  seconds: number;
+}
+
+function isVSCodeRunning(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    exec(
+      "ps aux | grep 'Visual Studio Code' | grep -v grep",
+      (err: Error | null, stdout: string, stderr: string) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(stdout.length > 0);
+      }
+    );
+  });
+}
+
+function saveTimerState(state: TimerState) {
+  fs.writeFileSync(TIMER_FILE_PATH, JSON.stringify(state));
+}
+
+function loadTimerState(): TimerState {
+  if (fs.existsSync(TIMER_FILE_PATH)) {
+    return JSON.parse(fs.readFileSync(TIMER_FILE_PATH, "utf-8"));
+  }
+  return { seconds: 0 };
+}
+let timerState = loadTimerState();
+
+function startTimer() {
+  intervalId = setInterval(async () => {
+    const vscodeRunning = await isVSCodeRunning();
+    if (vscodeRunning) {
+      timerState.seconds++;
+      saveTimerState(timerState);
+
+      if (mainWindow) {
+        mainWindow.webContents.send("update-timer", timerState.seconds);
+      }
+    }
+  }, 1000);
+}
 
 function createWindow(): void {
   mainWindow = new BrowserWindow({
